07e20ecd66d9993df438156c040825257ae50cc9
Adrian Gurnett <adrian.gurnett@gmail.com>
2018-08-19 18:31:27 +0100

    adding code coverage report, updating powershell script

--- Application.UnitTests/codecoverage_build.ps1
+++
@@ -1,3 +0,0 @@
-﻿$path = Get-Location
-dotnet test $path"\Application.UnitTests.csproj"  /p:CollectCoverage=true
-pause
---
+++ Application.UnitTests/run_codecoverage.ps1
@@ -0,0 +1,12 @@
+﻿$projectPath = Get-Location
+$reportTool = "C:\users\"+$env:UserName+"\.nuget\packages\reportgenerator\3.1.2\tools\ReportGenerator.exe"
+
+Write-Host $projectPath
+
+#1. Run Code Coverage
+dotnet test $projectPath"\Application.UnitTests.csproj" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+
+#2. Generate Report
+Start-Process $reportTool "-reports:.\coverage.opencover.xml -targetdir:$projectPath\CodeCoverageReport"
+
+pause
